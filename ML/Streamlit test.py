d9c43574f4caa216e3c29f9602bd88121bdcf355
Valle <v.anderson@live.se>
2025-02-03 16:54:53 +0100

    update streamlit kod med path

--- ML/Streamlit test.py
+++ ML/Streamlit test.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 # Ladda in CSV-filen
-uploaded_file = st.file_uploader("Ladda upp en CSV-fil", type=["csv"])
+uploaded_file = st.file_uploader(r"C:\workspace\vallesportfolio\ML\news_data_example.csv", type=["csv"])
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
     st.write(df)  # Visa datan i tabellform
